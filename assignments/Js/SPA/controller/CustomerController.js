a6a0899edb587deac194542826e286de9376cad9
shanuka55 <shanukalakshan5555@gmail.com>
2022-10-11 22:36:04 +0530

    Content updated

--- assignments/Js/SPA/controller/CustomerController.js
+++ assignments/Js/SPA/controller/CustomerController.js
@@ -1,4 +1,16 @@
+
+$("#CustomerBtn").on('click',function () {
+    $("#SearchFieldID").focus();
+});
+
+
+
+
 $(window).on('keydown',function (event) {
 
-    if (event.key=='Tab');
+    if (event.key=='Tab'){
+       event.preventDefault();
+    }
+
+
 })
---
+++ assignments/Js/SPA/controller/ItemController.js
@@ -0,0 +1,3 @@
+$("#CustomerBtn").on('click',function () {
+    $("#SearchFieldID").focus();
+});
